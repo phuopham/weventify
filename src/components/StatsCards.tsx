93a58ff4cc74f7e722ed2b668a1dc6b747b57956
phuopham <108047552+phuopham@users.noreply.github.com>
2023-11-03 16:55:04 +0700

    add statscards

---
+++ src/components/StatsCards.tsx
@@ -0,0 +1,50 @@
+import { GetFormStats } from "@/actions/form";
+import { StatsCard } from "./StatsCard";
+import { LuView } from "react-icons/lu";
+import { FaWpforms } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+import { TbArrowBounce } from "react-icons/tb";
+
+export const StatsCards = async ({loading}: {loading:boolean}) => {
+    const data = await GetFormStats();
+
+    return (
+        <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            <StatsCard
+                title="Total visits"
+                icon={<LuView className="text-slate-600" />}
+                helperText="All time form visits"
+                value={data?.visits.toLocaleString() || ""}
+                loading={loading}
+                className="shadow-md shadow-slate-600"
+            />
+
+            <StatsCard
+                title="Total submissions"
+                icon={<FaWpforms className="text-slate-600" />}
+                helperText="All time form submissions"
+                value={data?.submissions.toLocaleString() || ""}
+                loading={loading}
+                className="shadow-md shadow-slate-600"
+            />
+
+            <StatsCard
+                title="Submission rate"
+                icon={<HiCursorClick className="text-slate-600" />}
+                helperText="Visits that result in form submission"
+                value={data?.submissionRate.toLocaleString() + "%" || ""}
+                loading={loading}
+                className="shadow-md shadow-slate-600"
+            />
+
+            <StatsCard
+                title="Bounce rate"
+                icon={<TbArrowBounce className="text-slate-600" />}
+                helperText="Visits that leaves without interacting"
+                value={data?.submissionRate.toLocaleString() + "%" || ""}
+                loading={loading}
+                className="shadow-md shadow-slate-600"
+            />
+        </div>
+    );
+}
